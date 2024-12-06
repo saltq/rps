b674e3b526ce1520fe5ac652a74e25adba5c4775
saltq <59333310+saltq@users.noreply.github.com>
2024-12-06 23:27:36 +0500

    add everything else

--- script.js
+++ script.js
@@ -1,10 +1,69 @@
-function playRound(){
+let rpsClicked = undefined; //current clicked figure state
+const rps = ["rock","paper","scissors"];
+let rockButton = document.getElementById("rock");
+let paperButton = document.getElementById("paper");
+let scissorButton = document.getElementById("scissors");
+let picholder = document.getElementById("picholder");
+let handcomp = document.getElementById("handcomp");
+let game = document.getElementById("game");
+let yes = document.getElementById("YES");
+rockButton.addEventListener("click", ()=>playRound(0));
+paperButton.addEventListener("click", ()=>playRound(1));
+scissorButton.addEventListener("click", ()=>playRound(2));
+yes.addEventListener("click", ()=>playGame());
+let gameStatus = document.getElementById("status");
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+function playRound(player){
+    computer = Math.floor(Math.random()*3);
+    console.log(rps[computer]);
+    winner=calculateWinner(computer,player);
+    console.log(winner);
+    hideInterface();
+    handcomp.src="think.gif";
+    gameStatus.innerHTML = "YOU CHOOSE " +rps[player]+ " COMPUTER IS THINKY";
+    sleep(1500).then(() => { 
 
+        if (winner==="LOSE"){
+            handcomp.src="fingerwave.gif";
+            gameStatus.innerHTML = "COMPUTER CHOOSES " +rps[computer]+ " YOU LOSE";
+        }
+        else if (winner==="WIN"){
+            handcomp.src="explosion.gif"
+            gameStatus.innerHTML = "COMPUTER CHOOSES " +rps[computer]+ " YOU WIN";
+        }
+        else {
+            handcomp.src="draw.gif";
+            gameStatus.innerHTML = "COMPUTER CHOOSES " +rps[computer]+ " ITS A DRAW";
+        }
+    });
+    
+}
+function getPlayerChoice(x){
+    console.log(x);
+    rpsClicked = x;
 }
 function getComputerChoice(){
-    let arr = ["rock","paper","scissors"];
-    q = Math.floor(Math.random()*3);
-    return arr[q];
+    
+}
+function calculateWinner(computer, player){
+    if ((computer === player)){
+        return "DRAW";
+    }
+    else if ((computer === 0 && player === 1) ||
+             (computer === 1 && player === 2) ||
+             (computer === 2 && player === 0)) {
+        return "WIN";
+    }
+    else return "LOSE";
+}
+function hideInterface(){
+    picholder.style.display="none";
+}
+function playGame(){
+    game.style.display="flex";
+    document.getElementById("title").style.display="none";
 }
 
 
