5202de2f560f1ca2e16a661d94c68d6a1604fc53
saltq <59333310+saltq@users.noreply.github.com>
2025-02-22 23:47:11 +0500

    add several rounds

--- script.js
+++ script.js
@@ -7,14 +7,47 @@ let picholder = document.getElementById("picholder");
 let handcomp = document.getElementById("handcomp");
 let game = document.getElementById("game");
 let yes = document.getElementById("YES");
-rockButton.addEventListener("click", ()=>playRound(0));
-paperButton.addEventListener("click", ()=>playRound(1));
-scissorButton.addEventListener("click", ()=>playRound(2));
+const wholeScreen = document.querySelector("body");
+//rockButton.addEventListener("click", ()=>playRound(0));
+//paperButton.addEventListener("click", ()=>playRound(1));
+//scissorButton.addEventListener("click", ()=>playRound(2));
 yes.addEventListener("click", ()=>playGame());
 let gameStatus = document.getElementById("status");
+let choice = document.getElementById("picholder");
+const compCounter = document.getElementById("computerScore");
+const playerCounter = document.getElementById("playerScore");
+let compscore = 0;
+let playerscore = 0;
+let endgame = 0;
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
+const again = document.getElementById("AGAIN");
+//const explosion = document.createElement("a");
+again.addEventListener('click', () => {
+    if (endgame){
+       
+        endgame = 0;
+        wholeScreen.style.backgroundImage = 'url("explosion.gif")';
+        game.style.display = "none";
+        sleep(700).then(() => {
+            wholeScreen.style.backgroundImage = "none";
+        });
+        
+    }
+    else {
+        again.style.display = "none";
+        showInterface();
+        gameStatus.textContent = "PICK YO SHIT YO"
+    }
+})
+choice.addEventListener('click', (e) => {
+    let target = e.target;
+    console.log(e.target.id);
+    playRound(rps.indexOf(e.target.id));
+
+});
+
 function playRound(player){
     computer = Math.floor(Math.random()*3);
     console.log(rps[computer]);
@@ -28,21 +61,38 @@ function playRound(player){
         if (winner==="LOSE"){
             handcomp.src="fingerwave.gif";
             gameStatus.innerHTML = "COMPUTER CHOOSES " +rps[computer]+ " YOU LOSE";
+            compscore++;
         }
         else if (winner==="WIN"){
             handcomp.src="explosion.gif"
             gameStatus.innerHTML = "COMPUTER CHOOSES " +rps[computer]+ " YOU WIN";
+            playerscore++;
         }
         else {
             handcomp.src="draw.gif";
             gameStatus.innerHTML = "COMPUTER CHOOSES " +rps[computer]+ " ITS A DRAW";
         }
+        compCounter.textContent = "COMP: " + compscore;
+        playerCounter.textContent = "P1: "+ playerscore;
+        if (compscore == 5 || playerscore == 5){
+            if (compscore>playerscore){
+                endgame = 1;
+                gameStatus.textContent = "COMPUTER WINS. SUCKER"
+                again.textContent = "CLEAR CUM?"
+            }
+            else {
+                gameStatus.textContent = "YOU WIN!!!! YEAAAAHHHHHHHH"
+                endgame = 1;
+                again.textContent = "CLEAR CUM?"
+        }
+    }
+        again.style.display = "flex";
     });
-    
+
 }
 function getPlayerChoice(x){
-    console.log(x);
-    rpsClicked = x;
+   // console.log(x);
+    //rpsClicked = x;
 }
 function getComputerChoice(){
     
@@ -60,6 +110,9 @@ function calculateWinner(computer, player){
 }
 function hideInterface(){
     picholder.style.display="none";
+}
+function showInterface(){
+    picholder.style.display="flex";
 }
 function playGame(){
     game.style.display="flex";
